c1480b3df0faeabec912c8de9feed830191b92db
ElRafa009 <rafaelpj09@gmail.com>
2022-12-05 18:54:32 -0600

    footer
    Agregue footer, cambie media query del navbar, cambie el logo y cree la pagina principal, faltan imagenes

--- index.php
+++ index.php
@@ -1,13 +1,125 @@
-
+<?php include "navbar.html"; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <title>Inicio</title>
+
+    <link rel="stylesheet" href="css/estilos-index.css">
+
 </head>
 <body>
-    
+<header>
+        <section class="textos-header">
+            <h1>Certifícate suavemente</h1>
+            <h2>Con nuestra página 100% confiable</h2>
+        </section>
+        <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+                <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path>
+            </svg></div>
+    </header>
+    <main>
+        <section class="contenedor bienvenida">
+            <h2 class="titulo">Bienvenidos aspirantes!</h2>
+            <div class="contenedor-bienvenida">
+                <img src="img/b1.jpg" alt="" class="img-bienvenida">
+                <div class="contenido-textos">
+                    <h3><span>1</span>Las mejores certificaciones</h3>
+                    <p>Las certificaciones que ofrecemos son validas en muchas organizaciones en todo el mundo</p>
+                    <h3><span>2</span>Los mejores precios</h3>
+                    <p>En suavecito encuentras los mejores precios del mercado, jamás encontraras una oportunidad así para conseguir esa ansiada certificación</p>
+                </div>
+            </div>
+        </section>
+        <section class="portafolio">
+            <div class="contenedor">
+                <h2 class="titulo">Clientes satisfechos</h2>
+                <div class="galeria-satis">
+                    <div class="imagen-satis">
+                        <img src="img/cl1.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl2.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl3.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl4.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl5.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl6.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl7.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                    <div class="imagen-satis">
+                        <img src="img/cl8.jpg" alt="">
+                        <div class="hover-satis">
+                            <img src="img/icon-hover.png" alt="">
+                            <p>Nuestro trabajo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="servicios">
+            <div class="contenedor">
+                <h2 class="titulo">Nuestros servicios</h2>
+                <div class="servicio-cont">
+                    <div class="servicio-cert">
+                        <img src="img/cert.jpg" alt="">
+                        <h3>Certificaciones</h3>
+                        <p>Ofrecemos certificaciones de distintos lenguajes de programación podras descubrir más en nuestra seccion de información de certificaciones</p>
+                    </div>
+                    <div class="servicio-cert">
+                        <img src="img/cert2.jpg" alt="">
+                        <h3>Validez</h3>
+                        <p>Tenemos reconocimiento en todo el mundo, por lo que en cualquier lugar que presentes nuestras certificaciones seguro serán válidas</p>
+                    </div>
+                    <div class="servicio-cert">
+                        <img src="img/cert3.png" alt="">
+                        <h3>Variedad</h3>
+                        <p>Contamos con una gran variedad de certificaciones de acuerdo a tus necesidades, con distintos lenguajes de programación para todo tipo de situación</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 </html>
+<?php include "footer.html"; ?>
