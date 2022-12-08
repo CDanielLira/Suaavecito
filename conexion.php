96d1ffa2bac6344eb6af69cc9db5859a4d572c80
FabianMaEs <fabmac865@gmail.com>
2022-12-08 15:56:22 -0600

    Cuentas de BD comentadas

--- conexion.php
+++ conexion.php
@@ -1,14 +1,21 @@
 <?php
     session_start(); 
-    $servidor='localhost';
+    /*$servidor='localhost';
     $cuentasql='id19971352_suave';
     $password=' Toros12!suavecito';
-    $bd='id19971352_suaavecito';
+    $bd='id19971352_suaavecito';*/
+
+    $servidor='localhost';
+    $cuentasql='root';
+    $password='';
+    $bd='bdprueba1';
 
     if(!empty($_SESSION['cuenta'])){
+        $cuenta = $_SESSION['cuenta'];
         $usuario = $_SESSION['cuenta'];
         $nombre = $_SESSION['nombre'];
         $correo = $_SESSION['correo'];
+        //$carrito = $_SESSION['carrito'];
     }
     else {
         $cuenta = "NO";
