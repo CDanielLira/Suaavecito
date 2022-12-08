53d0840c34356b2be7cb6f6edaa84d04553cbd3e
FabianMaEs <fabmac865@gmail.com>
2022-12-08 15:56:34 -0600

    Administrador y carrito

--- navbar.php
+++ navbar.php
@@ -13,8 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos-navbar.css">
     <link rel="shortcut icon" type="image/x-icon" href="img/logo.jpeg">
-    
-
+    <script src="https://kit.fontawesome.com/ade620492b.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -28,17 +27,37 @@
                 <li><a href="#">Tienda</a></li>
                 <li><a href="Acerca de.php">Acerca De</a></li>
                 <li><a href="Ayuda.php">Ayuda</a></li>
-                <li><a href="contacto.php" class="btn"><button>Contactanos</button></a></li>
+                <li><a href="contacto.php" class="btn"><button>Contáctanos</button></a></li>
                 <!-- Si no está en una sesión activa-->
                 <?php
-                    if(!empty($_SESSION['id'])) {
+                    if(!empty($_SESSION['id']) && $usuario != 'admin') {
                 ?>
                 <li>
-                    <div class="btn-group">
+                    <div class="btn-group btn">
                         <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <?php echo "¡Hola, " . $_SESSION['nombre'] . '!';?>
                         </button>
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu" style="background: #6e1313; text-align:center;">
+                            <li>
+                                <a href="logout.php">Salir de la cuenta</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li><i style="color: white;" class="fa-solid fa-cart-shopping">0</i></li>
+                <?php
+                    }
+                    else if(!empty($_SESSION['cuenta']) && $usuario == 'admin'){
+                ?>
+                <li>
+                    <div class="btn-group btn">
+                        <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?php echo "Administrador";?>
+                        </button>
+                        <ul class="dropdown-menu" style="background-color: #6e1313; text-align:center;">
+                            <li>
+                                <a href="administrador.php">Ver gráficas</a>
+                            </li>
                             <li>
                                 <a href="logout.php">Salir de la cuenta</a>
                             </li>
@@ -46,16 +65,16 @@
                     </div>
                 </li>
                 <?php
-                }
-                else {
-                    //echo "No se puede: " . $_SESSION['id'];
+                    }
+                    else {
+                        //echo "No se puede: " . $_SESSION['id'];
                 ?>
                 <li>
                     <div class="btn-group btn">
                         <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Inicia sesión
                         </button>
-                        <ul class="dropdown-menu" style="background-color: #000; text-align:center;">
+                        <ul class="dropdown-menu" style="background-color: #6e1313; text-align:center;">
                             <li>
                                 <div class="form-group text-center">
                                     <small><a href="ingresar.php" data-toggle="modal" data-target="#modalPassword" class="texto-oscuro">Ingresa a tu cuenta</a></small>
@@ -63,7 +82,7 @@
                             </li>
                             <li>
                                 <div class="form-group text-center">
-                                    <small>¿No tienes una cuenta? <a href="registrar.php" data-toggle="modal" data-target="#modalPassword" class="texto-oscuro">Registra una cuenta</a></small>
+                                    <small style="color: gray;">¿No tienes una cuenta? <a href="registrar.php" data-toggle="modal" data-target="#modalPassword" class="texto-oscuro">Registra una cuenta</a></small>
                                 </div>
                             </li>
                         </ul>
@@ -90,6 +109,5 @@
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
 </body>
 </html>
