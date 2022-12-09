f185bf354ecc1c6fcafdae2921668ca8377f2062
LEPEG0 <116929271+LEPEG0@users.noreply.github.com>
2022-12-08 20:33:57 -0600

    Ayuda Final

--- Ayuda.php
+++ Ayuda.php
@@ -1,163 +1,153 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ayuda</title>
-    <!-- CSS only -->
+    <link rel="stylesheet" href="css/estilos-ayuda.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"><!-- CSS only -->
-    <style>
-        #Pasos {
-            text-align: center;
-
-        }
-
-        h4 {
-            margin: 10px;
-        }
-
-        body {
-            background-color: #CCCCCC;
-            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpolygon fill='%23baa3a3' points='1600 160 0 460 0 350 1600 50'/%3E%3Cpolygon fill='%23a87a7a' points='1600 260 0 560 0 450 1600 150'/%3E%3Cpolygon fill='%23975252' points='1600 360 0 660 0 550 1600 250'/%3E%3Cpolygon fill='%23852929' points='1600 460 0 760 0 650 1600 350'/%3E%3Cpolygon fill='%23730000' points='1600 800 0 800 0 750 1600 450'/%3E%3C/g%3E%3C/svg%3E");
-            background-attachment: fixed;
-            background-size: cover;
-        }
-
-        pre {
-            display: block;
-            font-size: 1.17em;
-            font-weight: bold;
-        }
-
-    </style>
     <?php include "navbar.php"; ?>
 </head>
 
 <body>
-    <h1 style="font-weight: bold; text-align: center; margin: 20px;">AYUDA</h1>
-    <pre style="text-align: center; margin: 40px;">En este apartado de ayuda te diremos los pasos para comprar nustros productos, 
-    como tambien algunas preguntas precuentes y como contactaronos</pre>
-    <h2 style="text-align: center">PASOS PARA COMPRAR</h2>
-    <div id="Pasos">
-        <h4>--1--</h4>
-        <pre>Entre a nuestra pagina <a href="">SUAAVECITO</a> 
-        y seleccione los productos que le gusten</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
-            <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716.075.09.141.175.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0z" />
-        </svg>
-        <h4>--2--</h4>
-        <pre>Cuando tenga los productos seleccinados, haga click en 
-        "Añadir a carrito" todos los productos que compre</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag-fill" viewBox="0 0 16 16">
-            <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-        </svg>
-        <h4>--3--</h4>
-        <pre>Cuando ya este listo haga click 
-        en el carrito de compras</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </svg>
-        <h4>--4--</h4>
-        <pre>Precione continuar</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-        </svg>
-        <h4>--5--</h4>
-        <pre>Llene lo que se le indica, para que su compra se 
-        realice correctamente</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-2-back-fill" viewBox="0 0 16 16">
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5H0V4zm11.5 1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2zM0 11v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-1H0z" />
-        </svg>
-        <h4>--6--</h4>
-        <pre>Espere unos cuantos dias o semanas para que 
-        le llegue su pedido</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-car-front-fill" viewBox="0 0 16 16">
-            <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679c.033.161.049.325.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.807.807 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2H6ZM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17 1.247 0 3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z" />
-        </svg>
-        <h4>--7--</h4>
-        <pre>Disfrute</pre>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-laughing-fill" viewBox="0 0 16 16">
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5c0 .501-.164.396-.415.235C6.42 6.629 6.218 6.5 6 6.5c-.218 0-.42.13-.585.235C5.164 6.896 5 7 5 6.5 5 5.672 5.448 5 6 5s1 .672 1 1.5zm5.331 3a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5zm-1.746-2.765C10.42 6.629 10.218 6.5 10 6.5c-.218 0-.42.13-.585.235C9.164 6.896 9 7 9 6.5c0-.828.448-1.5 1-1.5s1 .672 1 1.5c0 .501-.164.396-.415.235z" />
-        </svg>
-    </div>
-
-    <h2 style=" text-align: center">PREGUNTAS FRECUENTES</h2>
-    <div class="accordion" id="accordionPanelsStayOpenExample" style="margin: 30px;">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    ¿NUESTRAS COMPRARS SON CONFIABLES?
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
-                    <strong>Esta pagina si es confiable.</strong> Pero porque solo somos un proyecto universitario.
+    <header class="header" id="inicio">
+        <img class="hamburguer" src="img/bx-menu-alt-right.svg" alt="">
+        <nav class="menu-navegacion">
+            <a href="#inicio">Introduccion</a>
+            <a href="#servicio">Valores</a>
+            <a href="#galeria">Galeria</a>
+            <a href="#expertos">Nosotros</a>
+        </nav>
+        <div class="contenedor head">
+            <h1 class="titulo">AYUDA</h1>
+            <p class="copy">Aqui podemos resolver algunas de sus preguntas, como tambien los pasos de compra</p>
+        </div>
+    </header>
+    <main>
+        <section class="services contenedor" id="servicio">
+            <h2 class="subtitulo">PASOS PARA LA COMPRA</h2>
+            <div class="contenedor-servicio">
+               <img src="img/AyudaImg.png" alt="">
+                <div class="checklist-servicio">
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">1</span></h3>
+                        <p>Entre a nuestra pagina <a href="index.php">SUAAVECITO</a>
+                            y seleccione los productos que le gusten</p>
+                    </div>
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">2</span></h3>
+                        <p>Cuando tenga los productos seleccinados, haga click en
+                            "Añadir a carrito" todos los productos que compre</p>
+                    </div>
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">3</span></h3>
+                        <p>Cuando ya este listo haga click
+                            en el carrito de compras</p>
+                    </div>
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">4</span></h3>
+                        <p>Precione continuar</p>
+                    </div>
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">5</span></h3>
+                        <p>Llene lo que se le indica, para que su compra se
+                            realice correctamente</p>
+                    </div>
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">6</span></h3>
+                        <p>Espere unos cuantos dias o semanas para que
+                            le llegue su pedido</p>
+                    </div>
+                    <div class="service">
+                        <h3 class="n-service"><span class="number">7</span></h3>
+                        <p>Disfrute</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                    ¿QUE METODOS TENGO PARA GARA?
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                <div class="accordion-body">
-                    <strong>Solo una por el momento</strong> El unico metodo de pago que tenemos es la tarjeta de credito.
+        </section>
+        <section class="gallery" id="galeria">
+            <h2 style=" text-align: center">PREGUNTAS FRECUENTES</h2>
+            <div class="accordion" id="accordionPanelsStayOpenExample" style="margin: 30px;">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            ¿NUESTRAS COMPRARS SON CONFIABLES?
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                        <div class="accordion-body">
+                            <strong>Esta pagina si es confiable.</strong> Pero porque solo somos un proyecto universitario.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            ¿QUE METODOS TENGO PARA GARA?
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                        <div class="accordion-body">
+                            <strong>Solo una por el momento</strong> El unico metodo de pago que tenemos es la tarjeta de credito.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            ¿SI LLEGAN NUESTROS PRODUCTOS?
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <strong>No.</strong> Porque solo es un proyecto univeritario.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            ¿VAN AGREGAR MAS PRODUCTOS?
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <strong>Si.</strong> Como apenas estamos empezando tenemos muy poquitos productos, pero iremos agrgando productos.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            ¿VENDEN A MENORES?
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <strong>No.</strong> Al hacer los pagos en tarjeta, el encargado de la tarejeta con la que pagas es el responsabe del alcohol que enviamos.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            ¿TIENEN UNA TIENDA FISICA?
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <strong>No.</strong> Por el momento todavia no.
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                    ¿SI LLEGAN NUESTROS PRODUCTOS?
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                <div class="accordion-body">
-                    <strong>No.</strong> Porque solo es un proyecto univeritario.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                    ¿VAN AGREGAR MAS PRODUCTOS?
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                <div class="accordion-body">
-                    <strong>Si.</strong> Como apenas estamos empezando tenemos muy poquitos productos, pero iremos agrgando productos.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                    ¿VENDEN A MENORES?
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                <div class="accordion-body">
-                    <strong>No.</strong> Al hacer los pagos en tarjeta, el encargado de la tarejeta con la que pagas es el responsabe del alcohol que enviamos.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                    ¿TIENEN UNA TIENDA FISICA?
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                <div class="accordion-body">
-                    <strong>No.</strong> Por el momento todavia no.
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php include "footer.html"; ?>
+        </section>
+    </main>
+    <script src="js/menu.js"></script>
+    <script src="js/lightbox.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
+<?php include "footer.html"; ?>
 </html>
