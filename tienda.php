1086c8aa8d873a7b1f56348414cb01b4c86b59e9
LEPEG0 <116929271+LEPEG0@users.noreply.github.com>
2022-12-09 19:29:48 -0600

    Tienda

--- tienda.php
+++ tienda.php
@@ -1,7 +1,7 @@
 <?php
     require "navbar.php";
     require_once "conexion.php";
-
+    
     $sql = 'select * from productos';
     $resultado = $conexion -> query($sql);
     if(!empty($_GET['vr'])) {
@@ -23,49 +23,50 @@
     else {
         $cat = "Todos";
     }
-
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
 <body>
     <p id="categ" hidden><?php echo $cat?></p>
     <div class="cate">
         <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <?php
+            <li class="nav-item">
+                <?php
                 if($cat == "Todos")
-                    echo "<a class='nav-link disabled'>Todos</a>";
+                    echo "<a class='btn btn-outline-danger' href='tienda.php?cat=Todos' role='button'>Todos</a>";
                 else
-                    echo "<a class='nav-link' href='tienda.php?cat=Todos'>Todos</a>";
+                    echo "<a class='btn btn-outline-danger' href='tienda.php?cat=Todos' role='button'>Todos</a>";
             ?>
-        </li>
-        <li class="nav-item">
-            <?php
+            </li>
+            <li class="nav-item">
+                <?php
                 if($cat == "Licores")
-                    echo "<a class='nav-link disabled'>Licores</a>";
+                    echo "<a class='btn btn-outline-danger' href='tienda.php?cat=Licores' role='button'>Licores</a>";
                 else
-                    echo "<a class='nav-link' href='tienda.php?cat=Licores'>Licores</a>";
+                    echo "<a class='btn btn-outline-danger' href='tienda.php?cat=Licores' role='button'>Licores</a>";
             ?>
-        </li>
-        <li class="nav-item">
-            <?php
+            </li>
+            <li class="nav-item">
+                <?php
                 if($cat == "Vinos")
-                    echo "<a class='nav-link disabled'>Vinos</a>";
+                    echo "<a class='btn btn-outline-danger' href='tienda.php?cat=Vinos' role='button'>Vinos</a>";
                 else
-                    echo "<a class='nav-link' href='tienda.php?cat=Vinos'>Vinos</a>";
+                    echo "<a class='btn btn-outline-danger' href='tienda.php?cat=Vinos' role='button'>Vinos</a>";
             ?>
-        </li>
+            </li>
         </ul>
-    </div>    
+    </div>
 
-<div class="container">
-    <br>
-    <br>
-    <div class="row">
-        <script> 
-            var array=[];
-        </script>
-        <?php
+    <div class="container">
+        <br>
+        <br>
+        <div class="row">
+            <script>
+                var array = [];
+
+            </script>
+            <?php
             $resultado = $conexion -> query($sql);
             while($fila = $resultado ->  fetch_assoc()){
                 $id = $fila['idproducto'];
@@ -76,20 +77,21 @@
                 $categoria = $fila['categoria'];
                 if($cat == "Todos" || $categoria == $cat) {
         ?>
-                    <script>
-                        array.push("<?php echo $id ?>");
-                    </script>
-                    <div class="card" style="width: 22rem;">
-                        
-                            <!--<img onclick="" class="card-img-top" width="250" height="250" src="data:imagen/jpeg;base64,<?php echo base64_encode($fila['imagen']);?>"/>-->
-                            <img id="<?php echo $id ?>" onclick="vista(this.id)" class="card-img-top" width="16rem" src="data:imagen/jpeg;base64,<?php echo base64_encode($fila['imagen']);?>"/>
-                            <div>
-                                <h5>$<?php echo $precio ?></h5>
-                                <p><?php echo $nombre ?></p>
-                                <?php
+            <script>
+                array.push("<?php echo $id ?>");
+
+            </script>
+            <div class="card" style="width: 22rem; margin: 0 auto;">
+
+                <!--<img onclick="" class="card-img-top" width="250" height="250" src="data:imagen/jpeg;base64,<?php echo base64_encode($fila['imagen']);?>"/>-->
+                <img id="<?php echo $id ?>" onclick="vista(this.id)" class="card-img-top" width="16rem" src="data:imagen/jpeg;base64,<?php echo base64_encode($fila['imagen']);?>" />
+                <div>
+                    <h5>$<?php echo $precio ?></h5>
+                    <p><?php echo $nombre ?></p>
+                    <?php
                                     echo "<h6>Disponibles: " . $existencia . "</h6>";
                                     if(!empty($_SESSION['id']) && $existencia > 0){
-                                        ?><button class="btn btn-primary" id="<?php echo $id ?>" onclick="agregar(this.id)">Agregar al carrito</button><?php
+                                        ?><button class="btn btn-outline-danger" id="<?php echo $id ?>" onclick="agregar(this.id)">Agregar al carrito</button><?php
                                     }
                                     else if(!empty($_SESSION['id']) && $existencia <= 0){
                                         ?><button class="btn btn-secondary">No disponible</button><?php
@@ -98,35 +100,36 @@
                                         ?><a href="ingresar.php" class="btn btn-secondary">Iniciar sesión</a><?php
                                     }
                                 ?>
-                            </div>
-                            
-                    <br>
-                    </div>              
-        <?php
+                </div>
+
+                <br>
+            </div>
+            <?php
                 }//fin if
             }//fin while
         ?>
+        </div>
+        <br>
+        <br>
     </div>
-    <br>
-    <br>
-</div>
 </body>
 
 
 
-<script>   
-    function agregar(id){
+<script>
+    function agregar(id) {
         var ind = parseInt(id);
         //window.print(id);
         var cate = document.getElementById('categ').innerHTML;
-        window.location.href = "tienda.php?vr=" + ind + "&cat=" + cate;
+        window.location.href = "https://suaavecitoo.000webhostapp.com/tienda.php?vr=" + ind + "&cat=" + cate;
     }
-    
-    function vista(id){
+
+    function vista(id) {
         var ind = parseInt(id);
         //window.print(id);
-        window.location.href = "vistaprod.php?prod=" + ind;
+        window.location.href = "https://suaavecitoo.000webhostapp.com/vistaprod.php?prod=" + ind;
     }
+
 </script>
 
 <?php
